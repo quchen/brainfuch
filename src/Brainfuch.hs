63f6f5e5596c918ef58c5ff41157210cf7f55323
David Luposchainsky <dluposchainsky@gmail.com>
2013-11-23 22:00:21 +0100

    Command line parameter interface

--- src/Brainfuch.hs
+++ src/Brainfuch.hs
@@ -1,20 +1,50 @@
 -- | Frontend for the Brainfuck interpreter.
 
-module Brainfuch (runIO) where
+module Brainfuch (runIO, runString) where
 
 import Compile  as C
 import Parser   as P
 import Optimize as O
 import Types    as T
 
+import Data.DList
+import Control.Monad.Writer
+import System.IO as IO
+
+import Debug.Trace
+
 
 -- | Runs a given Brainfuck program in IO.
-runIO :: String -> IO ()
+runIO :: String  -- ^ Source
+      -> IO ()
 runIO = runIO' . prepare
       where runIO' (Pure _) = return ()
             runIO' (Roll x) = case x of
-                  ReadChar    cont -> getChar >>= runIO' . cont
-                  PrintChar c cont -> putChar c >> runIO' cont
+                  ReadChar    cont -> IO.getChar >>= runIO' . cont
+                  PrintChar c cont -> IO.putChar c >> runIO' cont
+
+
+
+runString :: String -- ^ Program input
+          -> String -- ^ Source
+          -> String
+runString input = toList . execWriter . runW input . prepare
+
+      where -- Run using Writer+DList
+
+            runW _ (Pure _) = do
+                  return ()
+
+            runW (i:is) (Roll (ReadChar cont)) = do
+                  runW is (cont i)
+
+            runW is (Roll (PrintChar c cont)) = do
+                  tell (singleton c)
+                  runW is cont
+
+            runW [] (Roll (ReadChar _cont)) = do
+                  tell (fromList "<unexpected end of input>")
+
 
 
 prepare :: String -> Program ()
--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,7 @@ module Main (main) where
 
 import System.Environment
 import System.IO
+import Data.List
 
 import qualified Brainfuch
 
@@ -13,13 +14,25 @@ main = do
       hSetBuffering stdout NoBuffering
       hSetBuffering stdin  NoBuffering
       case args of
-            ("--help":_) -> showHelp
-            ("-h":_)     -> showHelp
-            []           -> getContents   >>= Brainfuch.runIO
-            (file:_)     -> readFile file >>= Brainfuch.runIO
+            ("--help":_)   -> showHelp
+            ("-h":_)       -> showHelp
+            []             -> getContents >>= Brainfuch.runIO
+            xs@((':':_):_) -> print (fromText xs)
+            (file:_)       -> readFile file >>= Brainfuch.runIO
 
 
 showHelp :: IO ()
 showHelp = do
-      putStrLn "Usage: ./brainfuch [source]"
-      putStrLn "If no source is specified, read from STDIN"
+      putStrLn "Read from file:         ./brainfuch <source>"
+      putStrLn "Read from command line: ./brainfuch :<source>:<program input>"
+      putStrLn "Read from STDIN:        ./brainfuch"
+
+
+
+-- | Used to run brainfuck programs given using only command line parameters.
+fromText xs =
+      let (source, input) = splitOn ':' . drop 1 $ intercalate " " xs
+      in  Brainfuch.runString input source
+
+-- | Split a list on a certain element. The splitting element is dropped.
+splitOn c xs = (takeWhile (/= c) xs, drop 1 $ dropWhile (/= c) xs)
